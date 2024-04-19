9b87b4ff256c6653794696271791db086645fb27
wangfujie <wangfujie@softsafe-tech.com>
2024-04-19 16:36:19 +0800

    test

--- src/main/java/com/wang/java_jdk17/TestMain.java
+++ src/main/java/com/wang/java_jdk17/TestMain.java
@@ -1,5 +1,7 @@
 package com.wang.java_jdk17;
 
+import com.wang.java_jdk17.enums.FruitEnum;
+
 /**
  * 描述
  *
@@ -16,6 +18,29 @@ public class TestMain {
               "address": "北京市西城区"
             }
             """;
+        System.out.println(text);
+
+        withYield(FruitEnum.APPLE);
+
+        record TestRecord(String name,int age){
+            TestRecord{
+                System.out.println("执行record类构造器：name=" + name + ", age=" + age);
+            }
+        }
+
+        TestRecord testRecord = new TestRecord("zhangsan", 18);
+        System.out.println(testRecord);
+    }
+
+    private static void withYield(FruitEnum fruit) {
+        String text = switch (fruit) {
+            case APPLE, PEAR -> {
+                System.out.println("给的水果是: " + fruit);
+                yield "普通水果";
+            }
+            case MANGO, AVOCADO -> "进口水果";
+            default -> "未知水果";
+        };
         System.out.println(text);
     }
 }
---
+++ src/main/java/com/wang/java_jdk17/enums/FruitEnum.java
@@ -0,0 +1,15 @@
+package com.wang.java_jdk17.enums;
+
+/**
+ * 描述
+ *
+ * @author wangfj
+ * @date 2024/4/19
+ */
+public enum FruitEnum {
+    APPLE,
+    PEAR,
+    MANGO,
+    AVOCADO,
+    ;
+}
