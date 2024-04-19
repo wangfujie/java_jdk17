8c7a961963fb0fa6bb52a0be140dd94c805800c0
wangfujie <wangfujie@softsafe-tech.com>
2024-04-19 14:07:58 +0800

    新增

---
+++ src/main/java/com/wang/java_jdk17/TestMain.java
@@ -0,0 +1,21 @@
+package com.wang.java_jdk17;
+
+/**
+ * 描述
+ *
+ * @author wangfj
+ * @date 2024/4/19
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        String text = """
+            {
+              "name": "小黑说Java",
+              "age": 18,
+              "address": "北京市西城区"
+            }
+            """;
+        System.out.println(text);
+    }
+}
